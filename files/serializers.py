8bd21f359c8f05b5e9c4cff2d4886c631925b622
jm choi <527wlals@gmail.com>
2022-04-20 20:51:24 +0900

    add: FileSerializer, UserSerializer

--- files/serializers.py
+++ files/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from .models import File
+from django.contrib.auth.models import User
 
-class FileSerializer(serializers.Serializer):
+class FileSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
+    # owner = serializers.StringRelatedField()
 
     class Meta:
         model = File
         fields = '__all__'
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    files = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='file-detail')
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'files']
