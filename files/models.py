88506709f192365af4338ab1027763f6a19e30fd
jm choi <527wlals@gmail.com>
2022-04-21 00:01:58 +0900

    update: File model

--- files/models.py
+++ files/models.py
@@ -5,17 +5,18 @@ from django.db import models
 class File(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField(upload_to='uploads/', blank=True)
     is_folder = models.BooleanField(default=False)
-    size = models.IntegerField(blank=True)
-    expires_date = models.DateTimeField(blank=True)
+    size = models.IntegerField(blank=True, null=True)
+    expires_date = models.DateTimeField(blank=True, null=True)
     owner = models.ForeignKey('auth.User', related_name='files', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        self.size = self.file.size
+        if self.is_folder == False:
+            self.size = self.file.size
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return self.name
