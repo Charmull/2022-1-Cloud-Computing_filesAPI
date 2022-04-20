67c3ab8f6aa612ce303dd969ee8a39c2fb6fa865
jm choi <527wlals@gmail.com>
2022-04-20 20:53:14 +0900

    add: file and user viewsets

--- files/views.py
+++ files/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import File
+from .serializers import FileSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'files': reverse('file-list', request=request, format=format)
+    })
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
