1defc9adcb8fd9bedf79d7287ab96818dfd64e01
jm choi <527wlals@gmail.com>
2022-04-20 20:49:51 +0900

    update: File model

--- files/models.py
+++ files/models.py
@@ -9,6 +9,7 @@ class File(models.Model):
     is_folder = models.BooleanField(default=False)
     size = models.IntegerField(blank=True)
     expires_date = models.DateTimeField(blank=True)
+    owner = models.ForeignKey('auth.User', related_name='files', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
