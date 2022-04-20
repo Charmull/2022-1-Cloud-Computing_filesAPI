16bebda6037aedfcc22f53683022e6c0b5d42e4e
jm choi <527wlals@gmail.com>
2022-04-21 00:03:14 +0900

    update: FileSerializer, UserSerializer

--- files/serializers.py
+++ files/serializers.py
@@ -2,13 +2,20 @@ from rest_framework import serializers
 from .models import File
 from django.contrib.auth.models import User
 
+    # field_list = ['url', 'id', 'name', 'content', 'is_folder', 'expires_date', 'created_at', 'modified_at', 'owner', 'file', 'size', 'expires_date']
+
 class FileSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
     # owner = serializers.StringRelatedField()
 
+    # def get_is_folder(self, obj):
+    #     if obj.is_folder == False:
+    #         global field_list
+    #         field_list += ['file', 'size', 'expires_date']
+
     class Meta:
         model = File
-        fields = '__all__'
+        fields = ['url', 'id', 'name', 'content', 'is_folder', 'expires_date', 'created_at', 'modified_at', 'owner', 'file', 'size', 'expires_date']
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
